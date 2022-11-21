8d6c53302a001b950d756874c1a3b3e899bd67dd
CaiLiu <liucaissr@gmail.com>
2022-11-21 17:37:01 +0100

    Create maven project

---
+++ src/test/java/de/allianz/CAI_1121/Cai1121ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.CAI_1121;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cai1121ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
